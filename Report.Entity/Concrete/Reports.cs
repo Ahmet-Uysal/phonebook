572c437483aeeba69696beb58875ee3b4ae71be6
Ahmet Uysal <ahmetuysalbusiness@gmail.com>
2022-04-12 04:47:57 +0300

    ReportService added

---
+++ Report.Entity/Concrete/Reports.cs
@@ -0,0 +1,14 @@
+
+
+namespace Report.Entity.Concrete
+{
+
+    public class Reports
+    {
+
+        public string? Id { get; set; }
+        public string ReportDate { get; set; }
+        public string ReportState { get; set; }
+        public string Path { get; set; }
+    }
+}
