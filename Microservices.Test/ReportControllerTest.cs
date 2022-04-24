36c0383f3f08b36ea060e26b1a58fef7d99d5d38
ahmet-uysal <ahmetuysalbusiness@gmail.com>
2022-04-24 18:22:33 +0300

    ReportControllerTest Added

---
+++ Microservices.Test/ReportControllerTest.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Report.DataAccess.UnitofWork.Abstract;
+using Report.DataAccess.UnitOfWork.Concrete;
+using Report.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microservices.Test
+{
+    public class ReportControllerTest
+    {
+        [Fact]
+        public async Task GetFirstTest()
+        {
+            var application = new ReportTestHost();
+            var client = application.CreateClient();
+            var response = await client.GetStringAsync("/Report/GetFirst");
+            var resultObject = JsonConvert.DeserializeObject<Reports>(response);
+            IUnitOfWork a = new UnitOfWork();
+            var expected = (List<Reports>)await a.ReportRepository.GetAll();
+            Assert.Equal(expected.FirstOrDefault().Id, resultObject.Id);
+
+        }
+        [Fact]
+        public async Task GetAllReportsTest()
+        {
+            var application = new ReportTestHost();
+            var client = application.CreateClient();
+            var response = await client.GetStringAsync("/Report/GetAllReports");
+            var resultObject = JsonConvert.DeserializeObject<List<Reports>>(response);
+            IUnitOfWork a = new UnitOfWork();
+            var expected = (List<Reports>)await a.ReportRepository.GetAll();
+            Assert.Equal(expected.Count, resultObject.Count);
+        }
+    }
+}
